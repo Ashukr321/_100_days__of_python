a33213a1615a713ad7af4d4973fe3ff4a196ca9c
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-20 11:41:20 +0530

    day 1 is completed

---
+++ Day1/band_name_generator.py
@@ -0,0 +1,6 @@
+print("welcome to this  prject");
+
+name= input("enter your name \n");
+pet_name= input("enter your pet name \n");
+print("your name is : "+name+" and your pet name is : "+pet_name);
+
---
+++ Day1/variable.py
@@ -0,0 +1,7 @@
+#  python variables  
+name = "ashid";
+print(name);
+data = 344;
+print(len(name));
+
+# variables naming in the python 
