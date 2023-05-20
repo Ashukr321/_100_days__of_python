4595fab2d042698e73a6207f460853ff9a610668
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-20 09:28:41 +0530

    debuging and input is completed

---
+++ Day1/python_input.py
@@ -0,0 +1,4 @@
+# input function inthe python 
+# this is commetn 
+
+print("hello "+ input("what is your name?"));
---
+++ Day1/string_mani.py
@@ -0,0 +1,3 @@
+# string manupulation in the python 
+print("hello world\nhello world\nhello world");
+print("hello"+" Ashutosh ");
