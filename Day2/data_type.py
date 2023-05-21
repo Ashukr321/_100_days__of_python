67c18413295f9e4de0115a6b3925adefd23a3783
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-21 09:56:00 +0530

    data type in python is completed

---
+++ Day2/data_type.py
@@ -0,0 +1,27 @@
+# data type in python 
+# primitive data type in the python 
+#  number , string , float , integer 
+
+print("pritmitive data type in the python");
+# string 
+print("this is string data type ");
+
+#  integer 
+print(23);
+
+# float 
+print(23.4);
+
+#boolean
+print(True);
+
+#  type()function   this gives the type of the data 
+print("type() of function in the python ");
+print(type(23));
+print(type("this is string data type "));
+print(type(True));
+
+
+#  <class 'int'>
+# <class 'str'>
+# <class 'bool'>
